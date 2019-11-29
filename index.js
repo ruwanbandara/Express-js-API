795e69aaa18b2e5b6a45af08b47defa65b36ee26
ruwanbandara <ruwan.se.uok@gmail.com>
2019-11-29 21:36:23 +0530

    update api show not found id

--- index.js
+++ index.js
@@ -41,7 +41,15 @@ app.get('/api/members', (req, res) => { res.json(members);
 //get singale member
 
 app.get('/api/members/:id',(req, res) =>{
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+
+    }else{
+        res.status(400).json({mag: `No member with the id of ${req.params.id}`});
+
+    }
 
 });
 
